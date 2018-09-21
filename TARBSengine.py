beedbe5fbd50cd22dd9d4a3d458b1898e27092b4
Steve <stautonico@gmail.com>
2018-09-21 15:56:51 -0400

    added potion class w/ player updates
    Player Updates:
    * shield
    * usepotion() function
    * changed killplayer() to kill()
    
    Potion class:
    potion class contains:
    * name
    * health restoration amount

--- TARBSengine.py
+++ TARBSengine.py
@@ -23,11 +23,11 @@ class Player:
         self.weapon_one = None
         self.weapon_one_atk = 0
         self.shield = None
-        self.shiled_protect = 0
+        self.shield_protect = 0
 
     def editinv(self, item, amnt):
         self.inv[item] = amnt
-        debugout("Debugger: item given {} x {}".format(item, amnt))
+        debugout("Debugger: item given {} x {}".format(item.name, amnt))
 
     # Remove an item from the player
     def removeitem(self, item):
@@ -46,6 +46,10 @@ class Player:
         self.weapon_one = weapon
         self.weapon_one_atk = weapon.atk
 
+    def usepotion(self, potion):
+        self.hp += potion.hp
+        debugout("{} health restored, current health: {}".format(potion.hp, self.hp))
+
     # Make the player speak (more functionality planned)
     def say(self, text):
         print("{}: {}".format(self.name, text))
@@ -57,7 +61,7 @@ class Player:
         self.editinv(reward, 1)
 
     # noinspection PyMethodMayBeStatic
-    def killplayer(self, deathmessage, *endgame):
+    def kill(self, deathmessage, *endgame):
         print(deathmessage)
 
         # Decides if the game should exit at death
@@ -98,3 +102,9 @@ class Weapon:
     def __init__(self, name, atk):
         self.name = name
         self.atk = atk
+
+
+class Potion:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
--- debug.py
+++ debug.py
@@ -1,17 +1,11 @@
 import TARBSengine
 
-villager = TARBSengine.NPC("Strange Villager")
-
 TARBSengine.debug = True
 
-villager.talkto(True, "This is some debug text")
-villager.talkto(False, "This is laskdjaskldjl debug text")
+player = TARBSengine.Player("Steven", 10, 5, 100)
 
+hp = TARBSengine.Potion("HP Potion", 5)
 
-sword = TARBSengine.Weapon("Devil Sword", 5)
+player.editinv(hp, 1)
 
-zombie = TARBSengine.Enemy("Evil zombie", 10, 5, 10)
-player = TARBSengine.Player("steve", 5, 1, 1)
-player.equiptweapon(sword)
-print(player.weapon_one.name, player.weapon_one_atk)
-player.atk(zombie)
+player.usepotion(hp)
