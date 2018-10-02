1ecde13777f7497f6f62673e2a21aa97e1596aa7
Steve <stautonico@gmail.com>
2018-10-02 01:03:11 -0400

    Release Alpha 0.2.0
    Changelog: https://tautonico-enterprises.gitbook.io/tarbs/home/changelog

--- TARBSengine.py
+++ TARBSengine.py
@@ -5,6 +5,7 @@ debug = False
 
 version = "Alpha 0.1.0"
 
+
 # Debug output
 def debugout(text):
     if debug:
@@ -13,28 +14,27 @@ def debugout(text):
         pass
 
 
+# noinspection PyMethodMayBeStatic
 class Player:
     def __init__(self, name, hp, maxhp, minatk, maxatk):
         self.name = name
         self.hp = hp
         self.maxhp = maxhp
-        self.inv = {}
         self.minAtk = minatk
         self.maxAtk = maxatk
-        self.inv = {}
         self.weapon_one = None
         self.weapon_one_atk = 0
         self.shield = None
         self.shield_protect = 0
 
-    def editinv(self, item, amnt):
-        self.inv[item] = amnt
+    def additem(self, item, amnt):
+        item.amnt += amnt
         debugout("Debugger: item given {} x {}".format(item.name, amnt))
 
     # Remove an item from the player
-    def removeitem(self, item):
-        self.inv.pop(item)
-        debugout("Debugger: removed {} from player".format(item))
+    def removeitem(self, item, amnt):
+        item.amnt -= amnt
+        debugout("Debugger: removed {} x {} from player".format(amnt, item.name))
 
     # Attak an enemy
     def atk(self, opponent):
@@ -47,12 +47,40 @@ class Player:
             del opponent
 
     def equipweapon(self, weapon):
-        self.weapon_one = weapon
-        self.weapon_one_atk = weapon.atk
+        if weapon.amnt > 0:
+            self.weapon_one = weapon
+            self.weapon_one_atk = weapon.atk
+            debugout("Equipped weapon: {}, Damage: {}".format(weapon.name, weapon.atk))
+            weapon.amnt -= 1
+        else:
+            print("You don't have any!")
 
     def equipshield(self, shield):
-        self.shield = shield.name
-        self.shield_protect = shield.defense
+        if shield.amnt > 0:
+            self.shield = shield.name
+            self.shield_protect = shield.defense
+            debugout("Equipped shield: {}, Damage reduction: {}".format(shield.name, shield.defense))
+            shield.amnt -= 1
+        else:
+            print("You don't have any!")
+
+    def unequipweapon(self, weapon):
+        if self.weapon_one:
+            Player.weapon_one = None
+            Player.weapon_one_atk = 0
+            weapon.amnt += 1
+            debugout("Debugger: {} unequipped.".format(weapon.name))
+        else:
+            debugout("Debugger: Player doesn't have a weapon equipped.")
+
+    def unequipshield(self, shield):
+        if self.shield:
+            Player.shield = None
+            Player.shield_protect = 0
+            shield.amnt += 1
+            debugout("Debugger: {} unequipped.".format(shield.name))
+        else:
+            debugout("Debugger: Player doesn't have a weapon equipped.")
 
     def usepotion(self, potion):
         if self.hp >= self.maxhp:
@@ -63,16 +91,29 @@ class Player:
                 self.hp = self.maxhp
 
             debugout("{} health restored, current health: {}".format(potion.hp, self.hp))
-            self.inv[potion] -= 1
+            potion.amnt -= 1
 
     # Make the player speak (more functionality planned)
     def say(self, text):
         print("{}: {}".format(self.name, text))
 
+    # Thanks Antti Haapala
+    def drawborder(self, string):
+        return (string[0 + i:26 + i] for i in range(0, len(string), 26))
+
+    def think(self, thought):
+
+        print('+-' + '-' * 26 + '-+')
+
+        for line in self.drawborder(thought):
+            print('| {0:^{1}} |'.format(line, 26))
+
+        print('+-' + '-' * 26 + '-+')
+
     # Open a lootbox with specified loot (in list form)
     def openchest(self, loot):
         reward = choice(loot)
-        debugout("Reward from chest: {}".format(reward))
+        debugout("Reward from chest: {}".format(reward.name))
         self.editinv(reward, 1)
 
     # noinspection PyMethodMayBeStatic
@@ -113,7 +154,7 @@ class NPC:
     def __init__(self, name):
         self.name = name
 
-    def talkto(self, text, *name,):
+    def talkto(self, text, *name, ):
         if name:
             print("{}: {}".format(self.name, text))
         else:
@@ -122,17 +163,21 @@ class NPC:
 
 class Weapon:
     def __init__(self, name, atk):
+        self.amnt = 0
         self.name = name
         self.atk = atk
 
 
 class Potion:
     def __init__(self, name, hp):
+        self.amnt = 0
         self.name = name
         self.hp = hp
 
 
 class Shield:
     def __init__(self, name, defense):
+        self.amnt = 0
         self.name = name
         self.defense = defense
+
--- home/changelog.md
+++ home/changelog.md
@@ -6,21 +6,27 @@
 This will be updated with the release of Alpha 0.2.0
 {% endhint %}
 
-## 15.0.0 - 2042-12-03
+## Alpha 0.2.0 - (date)
+
+### Changes
+* Completely changed how the inventory works
+    * The amount of items the player has is now contained in the class for each individual item.
+    * Example:
+        * ```python
+          print(sword.amnt)
+          # 1  
+          # Within the instance of the `Sword` class, the amnt var says that the player has 1 sword.
+          ```
+* Changed how inventory editing works:
+    * You now add items to the player's inventory with `Player.additem`.
+    * Items are now are subtracted from the player's inventory with `Player.removeitem`.
 
 ### Fixed
-
-* Removed humans, they weren't doing fine with animals.
-
-### Changed
-
-* Animals are now super cute, all of them.
-
-## 14.0.0 - 2042-10-06
+* Debug output for `Player.openchest` now returns the proper output.
+* One Shield/Weapon is now removed from the player's inventory when equipped.
+* Cleaned up the player class and removed unused vars.
 
 ### Added
-
-* Introduced animals into the world, we believe they're going to be a neat addition.
-
-
-
+* `Player.equipweapon` and `Player.equipshield` now have debug output.
+* You can now communicate player thoughts by using `Player.think`. Thanks Antti Haapala3
+* Added `Player.unequipweapon` and `Player.unequipshield` to unequip weapons and shields.
